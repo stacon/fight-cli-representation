536fac232be92aa88bb34b5ad1fe2328a16e0cd2
Constantinos Stathis <constantinos-@hotmail.com>
2020-10-10 18:24:36 +0300

    imported fight-log-generator as an npm package

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-const fightInfo = require('../fight-generator/');
-const representBattle = require('./src/components/Representation').default;
+const fightInfo = require("@stacon/fight-log-generator");
+const representBattle = require("./src/components/Representation").default;
 
-representBattle(fightInfo, {
-  fighterName1: 'Mike Tyson',
-  fighterName2: 'Muhammad Ali',
+representBattle(fightInfo(), {
+  fighterName1: "Mike Tyson",
+  fighterName2: "Muhammad Ali",
   timeInterval: 1000,
   withConsoleLogRefresh: true,
 });
